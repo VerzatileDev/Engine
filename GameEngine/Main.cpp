59380d2971b0d0f04452b4f8a202f578ad0f343e
VerzatileDev <lattb@coventry.ac.uk>
2022-04-13 01:55:41 +0300

    #4 Window Creation

--- GameEngine/Main.cpp
+++ GameEngine/Main.cpp
@@ -1,10 +1,81 @@
-#include <iostream>
 #include <glad/glad.h> // Include this before Glfw
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main(int argc, char** argv)
 {
-	
+
+    /* INITIALIZE / CONFIGURE glfw*/
+    /* https://www.glfw.org/docs/latest/window.html#window_hints - Referencing to under functionality*/
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #ifdef __APPLE__ // <-- For Mac Os
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+    
+    // Specify Window
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, " Game Engine Window Title ", NULL, NULL);
+
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad : function pointers check
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    
+    while (!glfwWindowShouldClose(window)) // checks at the start of each loop iteration if GLFW has been instructed to close
+    {
+        processInput(window);
+
+        /* Rendering Commands */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+
+
+        glfwSwapBuffers(window); // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved)
+        glfwPollEvents(); // checks if any events are triggered (like keyboard input or mouse movement events),
+    }
+
+    
+    glfwTerminate(); // Terminates glfw allocated resources.
 	return 0;
+}
+
+// Proccess user inputs 
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// When Window size is change by (Os Or User) Callback is exacuted here.
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
 }
